d34aebe87a76a6f1c2594ef1c4a857f200d328a6
William Ryuputra <williamryuputra@yahoo.co.id>
2021-08-26 13:47:54 -0400

    mod4 solution

---
+++ mod4_solution/SpeakHello.js
@@ -0,0 +1,16 @@
+(function (window){
+  var helloSpeaker = {};
+  var speakWord = "Hello";
+  helloSpeaker.speak = function speak(name) {
+    console.log(speakWord + " " + name);
+  }
+  window.helloSpeaker= helloSpeaker;
+  }
+
+  
+
+
+)(window);
+
+
+
